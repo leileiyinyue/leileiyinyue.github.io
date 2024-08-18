7f12a857969778b24076585e6593edebb1450aad
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2024-08-18 11:58:35 +0800

    Update lunbotu2.js

--- lunbotu2.js
+++ lunbotu2.js
@@ -6,7 +6,7 @@
             	vid.play()
             }
 			//       音频循环播放列表
-			var vlist = ["Mp3/爱就一个字.mp3", "Mp3/白月光.mp3", "Mp3/别怕我伤心.mp3", "Mp3/当我孤独时还可以抱你.mp3", "Mp3/难以抗拒你容颜.mp3", "Mp3/披着羊皮的狼.mp3", "Mp3/有一点动心.mp3", "Mp3/真的爱你 .mp3", "Mp3/我可以抱你吗.mp3", "Mp3/伤心酒杯.mp3", "Mp3/碎心石.mp3", "Mp3/梦醉荷塘.mp3","Mp3/我是否也在你心中.mp3","Mp3/一生无悔.mp3","Mp3/今夜雨蒙蒙.mp3","Mp3/下辈子不做女人.mp3" ];
+			var vlist = ["Black Knight.mp3", "Rulers of Our Lands.mp3", "Cornfield Chase.mp3", "Hit n Smash.mp3", "Mothership.mp3", "Behind Enemy Lines.mp3", "Adventure.mp3", "Evil Incoming.mp3" ];
 				var k = 0;
 			
 			//点击切换到下一个资源
