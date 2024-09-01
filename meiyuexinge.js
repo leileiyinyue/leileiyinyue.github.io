fc0a0f462721b0280c90f5ecbb20fc1b85d110ce
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2024-09-01 14:12:53 +0800

    Update meiyuexinge.js

--- meiyuexinge.js
+++ meiyuexinge.js
@@ -6,7 +6,7 @@
             	vid.play()
             }
 			//       音频循环播放列表
-			var vlist = ["Mp3/半生雪.mp3", "Mp3/辞九门回忆.mp3", "Mp3/等你等到我心痛.mp3", "Mp3/点歌的人.mp3", "Mp3/独一无二.mp3", "Mp3/饿狼传说.mp3", "Mp3/鬼迷心窍.mp3", "Mp3/狂狼.mp3", "Mp3/旧梦一场.mp3", "Mp3/夜之光.mp3", "Mp3/一路上有你.mp3", "Mp3/一千个伤心的理由.mp3","Mp3/相思风雨中.mp3","Mp3/吻别.mp3","Mp3/下山.mp3","Mp3/想和你去吹吹风.mp3","Mp3/一曲相思.mp3" ];
+			var vlist = ["Bit Bit Loop.mp3", "Backbeat.mp3", "Desert Fox.mp3", "Driving Concern.mp3", "Eye of Forgiveness.mp3", "Shenzhen Nightlife.mp3" ];
 				var k = 0;
 			
 			//点击切换到下一个资源
