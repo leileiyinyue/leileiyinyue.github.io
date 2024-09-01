5e90026e71384e53789a3be2a18e270ddfc4948e
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2024-09-01 13:19:20 +0800

    Update gedanliebiao.js

--- gedanliebiao.js
+++ gedanliebiao.js
@@ -6,7 +6,7 @@
             	vid.play()
             }
 			//       音频循环播放列表
-			var vlist = ["Mp3/Mood.mp3", "Mp3/she.mp3", "Mp3/白月光与朱砂痣.mp3", "Mp3/潮汐.mp3", "Mp3/到了这个年纪.mp3", "Mp3/飞鸟和蝉.mp3", "Mp3/海底.mp3", "Mp3/酒醉的蝴蝶.mp3", "Mp3/可可托海的牧羊人.mp3", "Mp3/来迟.mp3", "Mp3/浪子闲话.mp3", "Mp3/热爱105°C的你.mp3","Mp3/少年.mp3","Mp3/世界这么大还是遇见你.mp3","Mp3/听我说谢谢你.mp3","Mp3/微微.mp3","Mp3/笑柄.mp3","Mp3/燕无歇.mp3","Mp3/要你管.mp3","Mp3/夜夜夜漫长.mp3", ];
+			var vlist = ["Arpent.mp3", "Sciencenet Theme.mp3", "Gotta Keep On Movin.mp3", "Murder On The Bayou.mp3", "Sun Up Gunned Down.mp3", "Ambush in Rattlesnake Gulch.mp3", "Goodnightmare.mp3", "Arpent.mp3" ];
 				var k = 0;
 			
 			//点击切换到下一个资源
