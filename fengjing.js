bae59a16d6415ff5e66b6b2b5219a89847e350a4
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2024-09-07 11:59:25 +0800

    Add files via upload

---
+++ fengjing.js
@@ -0,0 +1,40 @@
+			//获取视频对象
+			var vid = document.getElementById("vd");
+            var vplay = document.getElementById("btn")
+            vplay.onclick = function(){
+            	vid.play()
+            }
+			//       音频循环播放列表
+			var vlist = ["Mp3/唉声叹气.mp3", "Mp3/爱的挽歌.mp3", "Mp3/高山低谷.mp3", "Mp3/理想对象.mp3", "Mp3/终身美丽.mp3"];
+			var k = 0;
+			
+			//点击切换到下一个资源
+			document.getElementById("btn2").onclick = function() {
+				k++;
+				if(k == vlist.length) {
+					k = 0;
+				}
+				vid.src = vlist[k];
+				vid.play();
+
+			}
+
+			//点击切换到上一个资源
+			document.getElementById("btn1").onclick = function() {
+				k--;
+				if(k < 0) {
+					k = vlist.length - 1;
+				}
+				vid.src = vlist[k];
+				vid.play();
+
+			}
+			var k = 0; //初始播放视频索引
+			vid.onended = function() {
+				k++;
+				if(k == vlist.length) {
+					k = 0;
+				}
+				vid.src = vlist[k];
+				vid.play();
+			}
