4edcb344a9c968b36c7e93e422ff8ed65bc10dd5
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2024-09-08 16:40:58 +0800

    Add files via upload

---
+++ gangqinqu3.js
@@ -0,0 +1,40 @@
+			//获取视频对象
+			var vid = document.getElementById("vd");
+            var vplay = document.getElementById("btn")
+            vplay.onclick = function(){
+            	vid.play()
+            }
+			//       音频循环播放列表
+			var vlist = ["Mp3/梦的传说.mp3", "Mp3/梦中的婚礼.mp3", "Mp3/梦中的鸟.mp3", "Mp3/秘密的庭院.mp3", "Mp3/命运.mp3", "Mp3/拿达的主题.mp3", "Mp3/秋日私语.mP3", "Mp3/柔和彩虹.mp3", "Mp3/伤感乐曲.mp3" ];
+			var k = 0;
+			
+			//点击切换到下一个资源
+			document.getElementById("btn2").onclick = function() {
+				k++;
+				if(k == vlist.length) {
+					k = 0;
+				}
+				vid.src = vlist[k];
+				vid.play();
+
+			}
+
+			//点击切换到上一个资源
+			document.getElementById("btn1").onclick = function() {
+				k--;
+				if(k < 0) {
+					k = vlist.length - 1;
+				}
+				vid.src = vlist[k];
+				vid.play();
+
+			}
+			var k = 0; //初始播放视频索引
+			vid.onended = function() {
+				k++;
+				if(k == vlist.length) {
+					k = 0;
+				}
+				vid.src = vlist[k];
+				vid.play();
+			}
