2f3a6d6a2b01b3c4f1e39159bf051d0982f62132
leileiyinyue <87592432+leileiyinyue@users.noreply.github.com>
2025-09-23 09:15:16 +0800

    Update lunbotu1.js

--- lunbotu1.js
+++ lunbotu1.js
@@ -1,14 +1,7 @@
 
 			//获取视频对象
 			var vid = document.getElementById("vd");
-             //黑屏时继续播放
-              document.addEventListener('visibilitychange', () => {
-      if (document.hidden &&!vid.paused) {
-        // 页面隐藏且音频正在播放时，不执行暂停操作，保持播放
-      } else if (!document.hidden && vid.paused) {
-        vid.play();
-      }
-    });                                   
+                               
             var vplay = document.getElementById("btn")
             vplay.onclick = function(){
             	vid.play()
@@ -47,5 +40,6 @@
 				vid.src = vlist[k];
 				vid.play();
 			}
+
 
 
